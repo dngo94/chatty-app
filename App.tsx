2f7f553a8f2bc6cf2c8c639322ccd8e007f68f59
Daisy Ngo <69218823+dngo94@users.noreply.github.com>
2021-11-19 17:14:42 -0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,37 +1,24 @@
-import * as React from 'react';
-import {Text ,View, StyleSheet} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function TabOneScreen()
-   return(
-      <View style= {styles.container}>
-      <Image source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'}} style={styles.image} />
-      <View>
-       <View style={styles.row}>
-         <Text  style={styles.name}> Elon Musk</Text>
-         <Text  style={styles.text}> 11:11 AM </Text> 
-      </View>
-       <Text style={styles.text}>Hola Hola  coca cola </Text> 
-      </View>
-      </View>
-      );
-   } 
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
 
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
 
-   const styles = StyleSheet.create({
-    container: {
-      flexDirection: 'row' ,
-      padding: 10, 
-    },
-    image: {
-     height: 60,
-     width: 60,
-     borderRadius: 30,
-     marginRight: 10,
-     },
-     
-    text: {
- 
-    fontSize: 30,
-    color: 'blue'
-    }
-  });
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
+}
+
