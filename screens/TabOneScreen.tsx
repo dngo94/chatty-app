7197951891f2dee2b5d37097ce57271289b2455a
LuisACamacho <35936092+LuisACamacho@users.noreply.github.com>
2021-11-19 17:25:15 -0800

    Update TabOneScreen.tsx

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Text ,View, StyleSheet} from 'react-native';
+import {Text ,View,Image, StyleSheet} from 'react-native';
 
 export default function TabOneScreen(){
    return(
